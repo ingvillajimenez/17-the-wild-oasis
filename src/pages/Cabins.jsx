1dac37e10bbcffbebf7f974f3125a4426ab1bded
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-19 10:05:03 -0600

    Setting Up Storage Buckets

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,6 +3,8 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getCabins } from "../services/apiCabins";
 
+/////////////////////////////
+// Setting Up Storage Buckets
 function Cabins() {
   useEffect(function () {
     getCabins().then((data) => console.log(data));
@@ -12,6 +14,10 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://bhkmccdyhtrvvvgkggnh.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+        alt="cabin-001"
+      />
     </Row>
   );
 }
