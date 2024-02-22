ef134ae5099d28d2e1b9a70703173e8057400c25
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-22 17:29:20 -0600

    Client-Side Sorting: Sorting Cabins

---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+
+import { useSearchParams } from "react-router-dom";
+import Select from "./Select";
+
+//////////////////////////////////////
+// Client-Side Sorting: Sorting Cabins
+function SortBy({ options }) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get("sortBy") || "";
+
+  function handleChange(e) {
+    searchParams.set("sortBy", e.target.value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <Select
+      options={options}
+      type="white"
+      value={sortBy}
+      onChange={handleChange}
+    />
+  );
+}
+
+export default SortBy;
