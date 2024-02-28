087f610171a2294b7913e8784b7a7baebe9c9028
Juan Villa <ing.villa.jimenez@gmail.com>
2024-02-28 16:43:19 -0600

    Final Touches + Fixing Bugs

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -86,10 +86,14 @@ function Menus({ children }) {
   );
 }
 
+//////////////////////////////
+// Final Touches + Fixing Bugs
 function Toggle({ id }) {
   const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
+
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -108,7 +112,7 @@ function Toggle({ id }) {
 
 function List({ id, children }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
 
